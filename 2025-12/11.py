a93d19d32b3a492f970eebf0960e3eaaa31a2c68
Ye Htet Aung <ygxan18@gmail.com>
2025-12-11 10:09:46 +0700

    11

---
+++ 2025-12/11.py
@@ -0,0 +1,8 @@
+# 3531. Count Covered Buildings
+# Link: https://leetcode.com/problems/count-covered-buildings/description/?envType=daily-question&envId=2025-12-11
+# Time: 20+ mins
+# Solved: No
+
+# TIL: find x max, y max, x min, y min, the mid x,y is the center of the rectangle, 
+# x > min x and x < max x
+# y > min y and y < max y

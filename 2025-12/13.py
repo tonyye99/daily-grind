41946e21c2145a02585edee61e3e2419d9d71abd
Ye Htet Aung <ygxan18@gmail.com>
2025-12-13 10:53:22 +0700

    13

---
+++ 2025-12/13.py
@@ -0,0 +1,9 @@
+# 3606. Coupon Code Validator
+# Link: https://leetcode.com/problems/coupon-code-validator/description/?envType=daily-question&envId=2025-12-13
+# Time: 40+ mins
+# Solved: Yes
+
+# TIL: I tripped at sorting. 
+# first sorty by lexicographical order
+# second sort by businessLine priority order, make it an array and map the priority "electronics":0, "grocery":1, "pharmacy":2, "restaurant":3}
+# and return the value(code) of the second sorted array

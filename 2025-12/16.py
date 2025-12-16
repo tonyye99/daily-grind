df2e39d47b4d8c6c53903f29970a55f334d1bca3
Ye Htet Aung <ygxan18@gmail.com>
2025-12-16 10:57:07 +0700

    16

---
+++ 2025-12/16.py
@@ -0,0 +1,6 @@
+# 3562. Maximum Profit from Trading Stocks with Discounts
+# Link: https://leetcode.com/problems/maximum-profit-from-trading-stocks-with-discounts/description/?envType=daily-question&envId=2025-12-16
+# Time: -
+# Solved: No 
+
+# TIL: It is a DP knapsack problem. 

1eba563a3eb0d9fed960544c5409bb5c77088c2d
Ye Htet Aung <ygxan18@gmail.com>
2025-12-26 11:14:26 +0700

    26

---
+++ 2025-12/26.py
@@ -0,0 +1,6 @@
+# 955. 3075. Maximize Happiness of Selected Children
+# Link: https://leetcode.com/problems/maximize-happiness-of-selected-children/?envType=daily-question&envId=2025-12-25
+# Time: 20+
+# Solved: Yes
+
+# TIL: sorted + greedy, can alos do with heap (try heap next time)

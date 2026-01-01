f312d6ee873129a6d54247561e9f813a49cf4ceb
Ye Htet Aung <ygxan18@gmail.com>
2026-01-01 13:16:36 +0700

    Happy new year 2026

---
+++ 2026-01/1.py
@@ -0,0 +1,8 @@
+# 66. Plus One
+# Link: https://leetcode.com/problems/plus-one/description/?envType=daily-question&envId=2026-01-01
+# Time: 11+
+# Solved: Yes
+
+# TIL: convert to int and add 1, then convert back to list.
+# Approach-2: convert to int in a for loop by num = num * 10 + digit then add 1
+# then convert back to list by converting num to string and append each digit to list by converting each char to int.

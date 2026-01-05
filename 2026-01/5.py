8152d6dc8e69a06c3615316e97d8089ce1fa863c
Ye Htet Aung <ygxan18@gmail.com>
2026-01-05 18:16:50 +0700

    5

---
+++ 2026-01/5.py
@@ -0,0 +1,7 @@
+# 1975. Maximum Matrix Sum
+# Link: https://leetcode.com/problems/maximum-matrix-sum/description/?envType=daily-question&envId=2026-01-05
+# Time: 15+ (but sneaked)
+# Solved: Yes
+
+# TIL: sovled by counting negatives, total and smallest.
+# if total is odd, need to subtract the smallest number twice because one for including it and one for it being negative.

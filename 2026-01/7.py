670a875c52a0b74eed9c4b74aa847301507fa686
Ye Htet Aung <ygxan18@gmail.com>
2026-01-09 00:15:03 +0700

    LFG

---
+++ 2026-01/7.py
@@ -0,0 +1,7 @@
+# 1339. Maximum Product of Splitted Binary Tree
+# Link: https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/description/?envType=daily-question&envId=2026-01-07
+# Time: 20+ (but sneaked)
+# Solved: Yes
+
+# TIL: solved with dfs. There is math formula (sum - x) * x
+# TODO: try with bfs
---
+++ 2026-01/8.py
@@ -0,0 +1,6 @@
+# 1458. Max Dot Product of Two Subsequences
+# Link: https://leetcode.com/problems/max-dot-product-of-two-subsequences/description/?envType=daily-question&envId=2026-01-08
+# Time: 40+ (but sneaked)
+# Solved: Yes
+
+# TIL: DP problem. pair with choices.

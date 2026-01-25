4929e9d615d2630b4045a76b21ec539ef6caebbe
Ye Htet Aung <ygxan18@gmail.com>
2026-01-26 00:55:56 +0700

    26 Let's fucking go

---
+++ 2026-01/25.py
@@ -0,0 +1,6 @@
+# 1877. Minimize Maximum Pair Sum in Array 
+# Link: https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/description/?envType=daily-question&envId=2026-01-24 
+# Time: 20+
+# Solved: Yes
+
+# TIL: need to pair the max and min of divided/sorted nums. Then find max of each pairs additions. 
---
+++ 2026-01/26.py
@@ -0,0 +1,6 @@
+# 1984. Minimum Difference Between Highest and Lowest of K Scores
+# Link: https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/description/?envType=daily-question&envId=2026-01-25 
+# Time: 20+
+# Solved: Yes
+
+# TIL: sliding window to kth element, super simple.  

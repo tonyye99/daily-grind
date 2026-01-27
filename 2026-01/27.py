a9903b422d08a472f954aa9063e047b58d4bc58d
Ye Htet Aung <ygxan18@gmail.com>
2026-01-27 22:51:31 +0700

    IDGAF still grinding

---
+++ 2026-01/27.py
@@ -0,0 +1,7 @@
+
+# 1200. Minimum Absolute Difference
+# Link: https://leetcode.com/problems/minimum-absolute-difference/description/?envType=daily-question&envId=2026-01-26
+# Time: 20+
+# Solved: Yes
+
+# TIL: sort + find min diff + pair in each steps.
